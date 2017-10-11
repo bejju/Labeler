41f67b5ad43959cce120a67aa7d3d97bafa0d5d9
bejju <praharsha.bejju@ggktech.com>
2017-10-11 18:35:59 +0530

    fixed issue

--- src/routes/TeachAndQA/index.js
+++ src/routes/TeachAndQA/index.js
@@ -4,15 +4,15 @@ export default (store) => ({
   path: '',
   getComponent(nextState, cb) {
     require.ensure([], (require) => {
-      const Home = require('./containers/HomeContainer').default
-      const reducer = require('./modules/home').default
+      const Home = require('./containers/TeachAndQAContainer').default
+      const reducer = require('./modules/teachandqa').default
 
-      injectReducer(store, { key: 'home', reducer })
+      injectReducer(store, { key: 'teachandqa', reducer })
 
       /*  Return getComponent   */
       cb(null, Home)
 
       /* Webpack named bundle   */
-    }, 'home')
+    }, 'teachandqa')
   }
 })
