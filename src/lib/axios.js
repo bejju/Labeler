6522968e47f5ca0c068464829e4ea9d98505a478
bejju <praharsha.bejju@ggktech.com>
2017-10-11 21:21:42 +0530

    adding changes for overview api call

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,26 +5,9 @@ const token = localStorage.getItem('access_token');
 const headers = {};
 
 if (token) {
-  headers['Authorization'] = `Token ${token}`;
+  headers['Authorization'] = "Basic " + btoa(token + ":x-auth-token")
 }
 
 var instance = axios.create({baseURL: config.api.url, headers})
-
-instance
-  .interceptors
-  .response
-  .use(function (response) {
-    return response;
-  }, function (error) {
-    if (error.response.status == config.HTTP_Status.UNAUTHORIZE && localStorage.getItem('access_token')) {
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('logged_user');
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('logged_user_role');
-      window.location.href = '/login';
-    }
-    console.log('*** This is not called ***');
-    return Promise.reject(error);
-  });
 
 export default instance
--- src/routes/TeachAndQA/components/TeachAndQA.js
+++ src/routes/TeachAndQA/components/TeachAndQA.js
@@ -3,6 +3,10 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 
 export default class TeachAndQA extends React.Component {
+    componentDidMount() {
+        this.props.SetRouteParams(this.props.location.query)
+        localStorage.setItem('access_token', this.props.location.query.apiToken)
+    }
     render() {
         return (
             <div>
